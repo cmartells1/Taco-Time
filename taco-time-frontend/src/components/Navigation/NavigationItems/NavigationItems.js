35d64b5a449e33d90b6470f0cb17004bc5bee5d0
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2021-01-15 11:35:33 -0700

    change

--- taco-time-frontend/src/components/Navigation/NavigationItems/NavigationItems.js
+++ taco-time-frontend/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import classes from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
+import { Dropdown } from 'react-bootstrap';
 
 const navigationItems = props => (
 	<ul className={classes.NavigationItems}>
 		<NavigationItem link='/'>Launch Screen</NavigationItem>
+		<Dropdown></Dropdown>
 		<NavigationItem link='/admin'>Administration Screen</NavigationItem>
 	</ul>
 );
