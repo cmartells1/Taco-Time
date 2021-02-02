e809565073d2af9655fa831a5aaf4281ba430417
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2021-02-02 14:02:38 -0700

    workking

--- taco-time-frontend/src/components/Navigation/NavigationItems/NavigationItems.js
+++ taco-time-frontend/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,14 +2,23 @@ import React from 'react';
 
 import classes from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
-import { Dropdown } from 'react-bootstrap';
 
 const navigationItems = props => (
-	<ul className={classes.NavigationItems}>
-		<NavigationItem link='/'>Launch Screen</NavigationItem>
-		<Dropdown></Dropdown>
-		<NavigationItem link='/admin'>Administration Screen</NavigationItem>
-	</ul>
+	<div>
+		<ul className={classes.NavigationItems}>
+			<NavigationItem link='/'>Launch Screen</NavigationItem>
+			<NavigationItem link='/project-management'>
+				Project Management
+			</NavigationItem>
+			<NavigationItem link='/team-management'>Team Management</NavigationItem>
+			<NavigationItem link='/employee-allocation'>
+				Employee Allocation
+			</NavigationItem>
+			<NavigationItem link='/employee-management'>
+				Employee Management
+			</NavigationItem>
+		</ul>
+	</div>
 );
 
 export default navigationItems;
